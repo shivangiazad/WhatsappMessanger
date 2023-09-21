91f23ca6c0598cd41bee7ec23027027693bef60c
shivangiazad <145677021+shivangiazad@users.noreply.github.com>
2023-09-21 18:19:13 +0530

    Update pythonmessanger.py

--- pythonmessanger.py
+++ pythonmessanger.py
@@ -1,27 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pywhatkit
 pywhatkit.sendwhatmsg("+917398342916", " good evening sir", 17, 29)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
 
