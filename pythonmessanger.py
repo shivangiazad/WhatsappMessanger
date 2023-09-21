20e1aea700eb951fc6f0677c8d2107d720315a0a
shivangiazad <145677021+shivangiazad@users.noreply.github.com>
2023-09-21 18:18:30 +0530

    Add files via upload

---
+++ pythonmessanger.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pywhatkit
+pywhatkit.sendwhatmsg("+917398342916", " good evening sir", 17, 29)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
